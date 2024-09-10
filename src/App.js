dc62503b608c9ef4a94598b7b07d1577bcea666b
DanD <bren.dandxyz@gmail.com>
2024-09-10 11:59:42 +1000

    added tailwind and postcss

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import logo from './logo.svg';
+// src/components/MyComponent.jsx
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello, World!</h1>
+      
     </div>
   );
 }
 
+const MyComponent = () => {
+  return (
+    <div className="bg-blue-500 text-white p-4">
+      This is a Tailwind CSS styled component.
+    </div>
+  );
+};
+
 export default App;
+export {MyComponent};
