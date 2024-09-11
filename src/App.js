461fcf6a40282a5d1624abfb0d38a031ec76c313
DanD <bren.dandxyz@gmail.com>
2024-09-11 15:06:27 +1000

    moved component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 // src/components/MyComponent.jsx
 import React from 'react';
+import MyComponent from './MyComponent';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello, World!</h1>
-      
-    </div>
-  );
-}
-
-const MyComponent = () => {
-  return (
-    <div className="bg-blue-500 text-white p-4">
-      This is a Tailwind CSS styled component.
+      <MyComponent />
     </div>
   );
 };
 
 export default App;
-export {MyComponent};
